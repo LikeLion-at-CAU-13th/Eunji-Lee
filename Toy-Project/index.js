32808ad99f4871edc8cf330478fd6e2b3820ae64
eunjilee0311 <leg040311@gmail.com>
2025-05-02 23:44:35 +0900

    Upload Toy-Project files

---
+++ Toy-Project/index.js
@@ -0,0 +1,101 @@
+const BASE_URL = "http://my-guestbook.o-r.kr/guestbook/";
+
+//방명록 전체 불러오기 (GET)
+async function fetchGuestbooks() {
+    const res = await fetch(BASE_URL);
+    const data = await res.json();
+    console.log(data);
+    if (data.status === 200) {
+        renderGuestBookList(data.data);
+    } else {
+        alert("방명록 불러오기 실패");
+    }
+}
+
+//방명록 하나 저장하기 (POST)
+async function createGuestbook(name, title, content, password) {
+    const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, title, content, password}),
+        });
+
+    const data = await res.json();
+    if (data.status === 200) {
+        fetchGuestbooks();
+    } else {
+        alert("방명록 저장 실패");
+    }
+}
+
+//방명록 삭제하기 (DELETE)
+async function deleteGuestbook(id, password) {
+    const res = await fetch(`${BASE_URL}${id}/`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({password }),
+        });
+    
+    const data  = await res.json();
+    if (data.status === 200) {
+        fetchGuestbooks();
+    } else {
+        alert(data.message);
+    }
+}
+
+//화면에 랜더링
+function renderGuestBookList(entries) {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    entries.forEach((entry) => {
+        const div = document.createElement("div");
+        div.classList.add("entry");
+        div.innerHTML = `
+            <p><strong>${entry.name}</strong> <span style="color:gray;">(${entry.created})</span></p>
+            <p><strong>제목:</strong> ${entry.title}</p>
+            <hr>
+            <p><strong></strong> ${entry.content}</p>
+            <input type="password" class="delete-password" placeholder="비밀번호" />
+            <button class="delBtn" data-id="${entry.id}">삭제</button>
+        `;
+        list.appendChild(div);
+
+        const button = div.querySelector("button");
+        const input = div.querySelector("input");
+        button.addEventListener("click", () => {
+            const password=input.value;
+            deleteGuestbook(entry.id, password);
+        })
+    });
+}
+
+//저장 버튼 핸들링
+document.querySelector("#saveBtn").addEventListener("click", () => {
+    // 입력한 내용 가져오기
+    const name = document.getElementById("name").value;
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+    const password = document.getElementById("password").value;
+    
+    if (!name || !title || !content || !password) {
+        alert("모든 항목을 입력해주세요!");
+        return;
+    }
+
+    createGuestbook(name, title, content, password);
+
+    // 입력칸 초기화
+    document.getElementById("name").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("content").value = "";
+    document.getElementById("password").value = "";
+});
+
+//처음 실행 시 방명록 목록 불러오기
+window.onload = fetchGuestbooks;
