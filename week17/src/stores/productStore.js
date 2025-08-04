3569a400af697b57ada0f4af4d2f9d2b85df15aa
eunjilee0311 <leg040311@gmail.com>
2025-08-05 01:00:29 +0900

    불필요한 부분 삭제

--- week17/src/stores/productStore.js
+++ week17/src/stores/productStore.js
@@ -3,9 +3,6 @@ import { create } from "zustand";
 const useProductStore = create((set) => ({
     sortOrder: null,
     setSortOrder: (order) => set({ sortOrder: order }),
-    toggleSortOrder: () => set((state) => ({
-        sortOrder: state.sortOrder === 'low' ? 'high' : 'low'
-    })),
 }));
 
 export default useProductStore;
