95dd99c2b10933496b2b517fd94197bf443709aa
eunjilee0311 <leg040311@gmail.com>
2025-09-19 17:42:53 +0900

    feat: week19 과제

--- week19_practice/vite-practice/src/stores/productStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useProductStore = create((set) => ({
-    sortOrder: null,
-    setSortOrder: (order) => set({ sortOrder: order }),
-}));
-
-export default useProductStore;
