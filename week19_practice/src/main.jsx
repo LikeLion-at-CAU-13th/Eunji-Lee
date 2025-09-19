d43ef33739b91fbdea8cefbfe73609f25fc25bcf
eunjilee0311 <leg040311@gmail.com>
2025-09-19 16:36:33 +0900

    fix: 중첩 폴더 제거

--- week19_practice/src/main.jsx
+++ week19_practice/src/main.jsx
@@ -1,4 +1,3 @@
-// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,4 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
