5f95a55ea017bb7c09975b68a012be2006c47ffb
eunjilee0311 <leg040311@gmail.com>
2025-11-03 22:56:01 +0900

    feat: week27 과제

---
+++ week27/src/App.tsx
@@ -0,0 +1,230 @@
+import { useState } from "react";
+import { useMovieSearch } from "./hooks/useMovieSearch";
+import MovieCard from "./components/MovieCard";
+import styled from "styled-components";
+import MovieModal from "./components/MovieModal";
+import { useMovieDetail } from "./hooks/useMovieDetail";
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const { movies, status, error } = useMovieSearch(searchQuery);
+
+  const [selectedMovieId, setSelectedMovieId] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const {
+    movie,
+    loading: detailLoading,
+    error: detailError,
+  } = useMovieDetail(selectedMovieId);
+
+  const handleMovieSelect = (id: number): void => {
+    setSelectedMovieId(id);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovieId(null);
+  };
+
+  // 이벤트 핸들러 - Typescript 타입 자동 추론:
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <Homepage>
+      {/* 헤더 */}
+      <Header>
+        <Title>🎬 Search Movie 🎬</Title>
+        <Subtitle>전 세계의 영화 찾아보기</Subtitle>
+      </Header>
+
+      {/* 검색창 */}
+      <Search>
+        <SearchInput
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="영화 제목을 입력하세요..."
+        />
+      </Search>
+
+      {/* 상태별 UI */}
+      <Content>
+        {/* 로딩 */}
+        {status === "loading" && (
+          <StatusPage>
+            <Spinner />
+            <p>검색 중...</p>
+          </StatusPage>
+        )}
+
+        {/* 에러 */}
+        {status === "error" && (
+          <StatusPage>
+            <p> X {error} </p>
+          </StatusPage>
+        )}
+
+        {/* 영화 목록 */}
+        {status === "success" && (
+          <>
+            {movies.length > 0 ? (
+              <>
+                <div>
+                  <Result> 총 {movies.length}개의 영화를 찾았습니다 </Result>
+                </div>
+                <MovieGrid>
+                  {movies.map((movie) => (
+                    <MovieCard
+                      key={movie.id}
+                      movie={movie}
+                      onSelect={handleMovieSelect}
+                    />
+                  ))}
+                </MovieGrid>
+              </>
+            ) : (
+              <StatusPage>
+                <p>검색 결과가 없습니다.</p>
+              </StatusPage>
+            )}
+          </>
+        )}
+
+        {/* 초기 상태 */}
+        {status === "idle" && (
+          <StatusPage>
+            <p>🔎 영화 제목을 입력해서 검색해보세요! </p>
+          </StatusPage>
+        )}
+      </Content>
+
+      <MovieModal
+        movie={movie}
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+      />
+
+      {isModalOpen && detailLoading && (
+        <StatusModal onClick={handleCloseModal}>
+          <Spinner />
+          <p>로딩 중..</p>
+        </StatusModal>
+      )}
+
+      {isModalOpen && detailError && (
+        <StatusModal onClick={handleCloseModal}>
+          <p>🚨 {detailError}</p>
+        </StatusModal>
+      )}
+    </Homepage>
+  );
+}
+
+export default App;
+
+const Homepage = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  padding: 20px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: white;
+  margin-bottom: 40px;
+  padding: 20px;
+  gap: 15px;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  font-weight: 1200;
+  margin: 0;
+`;
+
+const Subtitle = styled.div`
+  font-size: 18px;
+  opacity: 0.9;
+  margin: 8px 0 0 0;
+`;
+
+const Search = styled.div`
+  max-width: 600px;
+  margin: 0 auto 40px;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  padding: 16px 20px;
+  font-size: 18px;
+  border: none;
+  border-radius: 50px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  outline: none;
+  transition: box-shadow 0.3s;
+  &:focus {
+    box-shadow: 0 6px 20px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const Content = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Result = styled.div`
+  color: white;
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 25px;
+  font-weight: 500;
+`;
+
+const MovieGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+`;
+
+const StatusPage = styled.div`
+  text-align: center;
+  padding: 60px 20px;
+  color: white;
+  font-size: 20px;
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-top-color: white;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  margin: 0 auto 20px;
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const StatusModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.8);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  font-size: 20px;
+  color: #393939ff;
+`;
